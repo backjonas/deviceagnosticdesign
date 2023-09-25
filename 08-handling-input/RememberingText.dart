e2ee1cd010403280f295d332444fcbc4b050eecc
Jonas Bäck <jonas.c.back@gmail.com>
2023-09-25 14:59:57 +0300

    Finish chapter 8

---
+++ 08-handling-input/RememberingText.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final stringProvider = StateProvider<String>((ref) => '');
+
+final routes = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: '/text',
+      builder: (context, state) => TextInputScreen(),
+    ),
+  ],
+);
+
+main() {
+  runApp(ProviderScope(child: MaterialApp.router(routerConfig: routes)));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ElevatedButton(
+            onPressed: () => context.go('/text'),
+            child: const Text("To input screen")));
+  }
+}
+
+class TextInputScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: [
+      MyTextField(),
+      ElevatedButton(
+        onPressed: () => context.go('/'),
+        child: const Text("To home screen"),
+      )
+    ]));
+  }
+}
+
+class MyTextField extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends ConsumerState<MyTextField> {
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() =>
+        ref.watch(stringProvider.notifier).update((state) => _controller.text));
+
+    WidgetsBinding.instance.addPostFrameCallback(
+        (_) => _controller.text = ref.watch(stringProvider));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(controller: _controller);
+  }
+}
