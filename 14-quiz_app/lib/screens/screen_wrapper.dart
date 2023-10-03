05ecf4b297b73b78e1f8896f2f55d795b5062681
Jonas Bäck <jonas.c.back@gmail.com>
2023-10-03 14:39:47 +0300

    Add IconButtons to top bar

--- 14-quiz_app/lib/screens/screen_wrapper.dart
+++ 14-quiz_app/lib/screens/screen_wrapper.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ScreenWrapper extends StatelessWidget {
   final Widget widget;
   const ScreenWrapper(this.widget);
+
+  _goTo(BuildContext context, String route) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Quiz app"),
-      ),
+          leading: IconButton(
+              icon: const Icon(Icons.home), onPressed: () => context.go('/')),
+          title: const Center(child: Text("Quiz app")),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                  icon: const Icon(Icons.auto_graph),
+                  onPressed: () => context.go('/stats')),
+            ),
+          ]),
       body: widget,
     );
   }
